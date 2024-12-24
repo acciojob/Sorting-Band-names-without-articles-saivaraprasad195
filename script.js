ded59d3b67c76e2c5351b40051352fc61e1d82f2
Sai Varaprasad <62549091+saivaraprasad195@users.noreply.github.com>
2024-12-24 16:11:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 //your code here
 
+let articles = ["the","an","a"];
+
+let sortedNames = touristSpots.map ((item) ={
+	let words = item.split(" ");
+	if( articles.includes( words[0])) words.shift();
+
+	return{ original : item , afterRemovingArticle : words.join(" ")};
+}).sort( (a,b) => a.afterRemovingArticle.localCompare(b.afterRemovingArticle))
+	.map( item => item.original);
+
+let ul = document.querySelector("ul");
+sortedNames.forEach( (item) => {
+	let li = document.createElement("li");
+	li.innerText = item;
+	ul.appendChild(li);
+})
